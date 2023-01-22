fe791591b2219e984082069394645865ac2b84a2
lakshay1001 <lakshay.kalra@bookvala.com>
2023-01-22 23:31:17 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div><nav >
+    this is Navbar
+  </nav></div>
+  )
+}
+
+export default Navbar
